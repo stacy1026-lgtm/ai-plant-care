4319787c15007a315f7ff416002682ec5f0df471
stacy1026-lgtm <stacy1026@gmail.com>
2026-01-22 22:46:18 -0500

    Update title from 'Mitzy's House Plants' to 'My Plant Garden'

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from datetime import date
 import pandas as pd
 
 st.set_page_config(page_title="Plant Tracker", page_icon="🌱")
-st.title("🌱 Mitzy's House Plants")
+st.title("🌱 My Plantg Garden")
 
 # 1. Connection to Google Sheets
 conn = st.connection("gsheets", type=GSheetsConnection)
