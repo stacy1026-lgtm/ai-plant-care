2f61cf7833839804e8cea94992d8855d172ba7b9
stacy1026-lgtm <stacy1026@gmail.com>
2026-02-14 10:29:18 -0500

    Added Dev Tag back.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from streamlit_gsheets import GSheetsConnection
 from datetime import date, timedelta, datetime
 import pandas as pd
 
-st.warning("⚠️ YOU ARE IN THE STAGING ENVIRONMENT")
+st.warning("⚠️ YOU ARE IN THE DEVELOPMENT ENVIRONMENT")
 # 1. Initialize Session State (at the very top)
 st.set_page_config(page_title="Plant Garden", page_icon="🪴")
 if 'water_expanded' not in st.session_state:
