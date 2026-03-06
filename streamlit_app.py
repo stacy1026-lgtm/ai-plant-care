70e51e50f91c8de09c458dcb63277b512616b218
stacy1026-lgtm <stacy1026@gmail.com>
2026-03-06 15:48:44 -0500

    AddedDevWarning.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ now_server = datetime.now() # Server defaults to UTC
 
 #st.divider()
 
-st.warning("⚠️ YOU ARE IN THE STAGING ENVIRONMENT")
+st.warning("⚠️ YOU ARE IN THE DEVELOPMENT ENVIRONMENT")
 # 1. Initialize Session State (at the very top)
 st.set_page_config(page_title="Plant Garden", page_icon="🪴")
 
